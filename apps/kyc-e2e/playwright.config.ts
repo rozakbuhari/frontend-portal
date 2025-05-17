d1feb5b9b3577c68828c83070110f21d4259d835
Rozak Buhari <fathurozak.buhari@frankieone.com>
2025-05-17 22:34:32 +0700

    chore: update e2e test configs to use production servers and unique ports

--- apps/kyc-e2e/playwright.config.ts
+++ apps/kyc-e2e/playwright.config.ts
@@ -3,7 +3,7 @@ import { nxE2EPreset } from '@nx/playwright/preset';
 import { workspaceRoot } from '@nx/devkit';
 
 // For CI, you may want to set BASE_URL to the deployed application.
-const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
+const baseURL = process.env['BASE_URL'] || 'http://localhost:4202';
 
 /**
  * Read environment variables from file.
@@ -24,8 +24,8 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npx nx run kyc:serve-static',
-    url: 'http://localhost:4200',
+    command: 'npx nx run kyc:serve:production',
+    url: 'http://localhost:4202',
     reuseExistingServer: true,
     cwd: workspaceRoot,
   },
--- apps/root-e2e/playwright.config.ts
+++ apps/root-e2e/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npx nx run root:serve-static',
+    command: 'npx nx run root:serve:production',
     url: 'http://localhost:4200',
     reuseExistingServer: true,
     cwd: workspaceRoot,
