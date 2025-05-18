50a4844cccb5b97cff3ded6b4603f6ab2249e407
Rozak Buhari <fathurozak.buhari@frankieone.com>
2025-05-18 12:57:42 +0700

    feat: create backend services for KYC, KYB and core functionality with e2e test setup

---
+++ apps/backend/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: '@frontend-portal/backend',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: 'test-output/jest/coverage',
+};
